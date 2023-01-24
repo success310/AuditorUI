0300818b761f157e69e52e8d1bcd031ebdbd6d93
success310 <victory.clever310@gmail.com>
2023-01-24 03:46:39 +0000

    README

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Auditor UI
+# Auditor: Smart Contract Auditing
+
+## Introduction
+
+The UI for the [Auditor](https://github.com/Keyrxng/a-dApp-aDay/blob/main/src/Auditor/Auditor.sol) protocol built by yours truly. It allows protocol's to submit smart contracts for a small fee which grows exponentially for every audit that is currently in the queue. Registered auditors can self-assign audits. Submission are made directly to the contract and all findings are stored on-chain.
+
+The interested thing 
+
+* * 
+
+1. Submit Smart Contracts for audit, paying the submission fee.
+2. Wait for Auditors to assign and conduct their audit process.
+3. Audit results are stored on-chain, the audit is finalized by the submitter when they are satisified by the findings.
+4. Auditor is paid based on their findings (The more findings, the higher the pay.) while the protocol accrues fees from submissions and auditor registrations.
+
+* *
+
+* * *
+
+### How to Use
+* *
+
+ 1. OpenSeason, Submissions can be made, auditors can be registered and audits can be assigned.
+ 2. Payments are semi-handled, findings are stored via mappings (<code>lows[AuditID][IssueNumber] = bytes("Findings")</code>).
+ 3. TBA as more functionality is built in and UI is updated.
+
+* *
+* * *
+
+### Notes
+
+* I understand storage costs and know that there are far far more gas effective ways to store this information, I'll explore other options eventually but for now, I'd rather not get mired in gas costs for the sake of a demo.
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,42 +1,47 @@
 import {
-	Container,
-	Grid,
-	Card,
-	CardMedia,
-	CardContent,
-	CardActions,
 	Typography,
-	Button,
-  } from "@mui/material";
+} from "@mui/material";
 import AuditSubmissionForm from "../components/auditForm";
-  
-  export default function Home() {
+
+export default function Home() {
 	return (
-	  <div >
-		<header className="px-4 py-8">
-		  <h1 className="text-3xl text-gray-900 font-medium text-center">Smart Contract Auditing Inc.</h1>
-  
-		  <Typography className="text-yellow-400 text-center px-4">
-		  Smart Contract Auditing (SCA) is a professional group of blockchain, smart contract, and distributed systems engineers who work together to develop solutions for blockchain-based applications. We provide services to teams that require blockchain contract auditing, verification, and code reviews. Our goal is to ensure that all smart contracts are secure and free from vulnerabilities, by providing expert contract security reviews, application security auditing, and contract verification services. With SCA, you can trust that your contracts are in good hands.
-		  </Typography>
-		  <div className="text-center">
-				<br/>
-		  </div>
-		</header>    
-		
-	  <AuditSubmissionForm />
+		<div>
+			<header className="px-4 py-8">
+				<h1 className="text-3xl text-gray-900 font-medium text-center">
+					Smart Contract Auditing Inc.
+				</h1>
 
-	  <footer className="py-4">
-    <Typography h className="text-lg text-gray-900 font-medium text-center">
-      Smart Contract Auditing Inc.
-    </Typography>
-    <Typography className="text-yellow-400 text-center">
-      We are dedicated to providing the best cybersecurity solutions for
-      blockchain-based applications.
-    </Typography>
-  </footer>
-</div>
-	)
-  }
+				<Typography className="text-yellow-400 text-center px-4">
+					Smart Contract Auditing (SCA) is a professional group of
+					blockchain, smart contract, and distributed systems
+					engineers who work together to develop solutions for
+					blockchain-based applications. We provide services to teams
+					that require blockchain contract auditing, verification, and
+					code reviews. Our goal is to ensure that all smart contracts
+					are secure and free from vulnerabilities, by providing
+					expert contract security reviews, application security
+					auditing, and contract verification services. With SCA, you
+					can trust that your contracts are in good hands.
+				</Typography>
+				<div className="text-center">
+					<br />
+				</div>
+			</header>
 
+			<AuditSubmissionForm />
 
+			<footer className="py-4">
+				<Typography
+					h
+					className="text-lg text-gray-900 font-medium text-center"
+				>
+					Smart Contract Auditing Inc.
+				</Typography>
+				<Typography className="text-yellow-400 text-center">
+					We are dedicated to providing the best cybersecurity
+					solutions for blockchain-based applications.
+				</Typography>
+			</footer>
+		</div>
+	);
+}
